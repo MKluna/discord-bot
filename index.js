12a1c2fc971e4457e47eda375d143ddda8761c3f
unknown <mkluna@institutosanmartin.edu.ar>
2020-07-27 10:50:18 -0300

    bot connection is implemented

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const botConfig = require('./config/botConfig.json');
 const {Client,MessageEmbed} = require("discord.js");
 const client = new Client();
+
+client.login(botConfig.token);
+
+client.on("ready", () => {
+    console.log(`¡The bot is ready to be used!`);
+    client.user.setStatus("online");
+    client.user
+      .setActivity("-helpbot", {type: "LISTENING"})
+      .then((presence) => console.log(`Activity set to ${presence.activities[0].name}`))
+      .catch(console.error);
+});
