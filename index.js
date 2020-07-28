a11545aa397565127e0d1cc6698fe0c9aa70b42e
unknown <mkluna@institutosanmartin.edu.ar>
2020-07-28 16:22:55 -0300

    Added in index Helpbot,SearchGif and DeltedMessage

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-
-const {Client} = require('discord.js');
+const { Client } = require("discord.js");
 const client = new Client();
-const botConfig = require('./config/botConfig.json');
-const alarma = require('./command/criptocurren')
+const botConfig = require("./config/botConfig.json");
+const alarma = require("./command/criptocurren");
+const helpbot = require("./command/helpbot");
+const searchGif = require("./command/searchGifs");
+const deleteMessages = require('./command/deleteMessages');
+const {prefix,prefixCript} = require('./config/botConfig.json');
+
 client.login(botConfig.token);
 
 client.on("ready", () => {
-    console.log(`¡The bot is ready to be used!`);
-    client.user.setStatus("online");
-    client.user
-      .setActivity("-helpbot", {type: "LISTENING"})
-      .then((presence) => console.log(`Activity set to ${presence.activities[0].name}`))
-      .catch(console.error);
+  console.log(`¡The bot is ready to be used!`);
+  client.user.setStatus("online");
+  client.user
+    .setActivity("-helpbot", { type: "LISTENING" })
+    .then((presence) =>
+      console.log(`Activity set to ${presence.activities[0].name}`)
+    )
+    .catch(console.error);
 
-    // CriptoCurrent
-      alarma(client,botConfig.prefixCript);
-
+  // CriptoCurrent
+  alarma(client,prefixCript);
+  //helpbot
+  helpbot(client,prefix);
+  //searchGif
+  searchGif(client,prefix)
+  //deleted Messages
+  deleteMessages(client,prefix)
 });
